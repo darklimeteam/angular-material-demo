b35309b04eac3a4de6a2000cda9ad6c5a2395c6c
JsssCode <nvplyuta@gmail.com>
2019-07-16 13:49:07 +0300

    basic components

---
+++ src/app/components/post-item/post-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from 'src/app/core/models/post.model';
+
+@Component({
+  selector: 'app-post-item',
+  templateUrl: './post-item.component.html',
+  styleUrls: ['./post-item.component.scss']
+})
+export class PostItemComponent implements OnInit {
+
+  @Input()
+  post: Post;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
